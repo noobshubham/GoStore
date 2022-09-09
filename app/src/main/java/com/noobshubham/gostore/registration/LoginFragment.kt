ff1ec16543abf1b1049f8aac81401a0b86d366b7
noobshubham <noobshubham65@gmail.com>
2022-09-09 17:30:33 +0530

    Changed GoogleSignInClient

--- app/src/main/java/com/noobshubham/gostore/registration/LoginFragment.kt
+++ app/src/main/java/com/noobshubham/gostore/registration/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.noobshubham.gostore.registration
 
 import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,24 +11,35 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
 import com.noobshubham.gostore.MapsActivity
 import com.noobshubham.gostore.R
 import com.noobshubham.gostore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private val REQ_ONE_TAP: Int = 300
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
+    // to verify firebase authentication
     private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var oneTapClient: SignInClient
+
+    // on tapping the button google will save the credentials
+    private lateinit var signInRequest: BeginSignInRequest
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,50 +56,52 @@ class LoginFragment : Fragment() {
 
         binding.tvSignUp.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_signupFragment) }
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
+        oneTapClient = Identity.getSignInClient(requireActivity())
+        // initiate the auth variable
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
             .build()
 
-        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        // auth button click listener
         binding.googleButton.setOnClickListener { signInWithGoogle() }
     }
 
     private fun signInWithGoogle() {
-        val signInIntent = googleSignInClient.signInIntent
-        launcher.launch(signInIntent)
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(requireActivity()) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    Snackbar.make(binding.root, e.message.toString(), Snackbar.LENGTH_INDEFINITE)
+                        .show()
+                }
+            }
+            .addOnFailureListener(requireActivity()) { e ->
+                Snackbar.make(binding.root, e.message.toString(), Snackbar.LENGTH_INDEFINITE).show()
+            }
     }
 
-    private val launcher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleResult(task)
-            }
-        }
-
-    private fun handleResult(task: Task<GoogleSignInAccount>) {
-        if (task.isSuccessful) {
-            val account: GoogleSignInAccount? = task.result
-            if (account != null)
-                updateUI(account)
-            else
-                Toast.makeText(context, task.exception.toString(), Toast.LENGTH_SHORT).show()
-        }
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser != null)
+            updateUI()
     }
 
-    private fun updateUI(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                val intent = Intent(context, MapsActivity::class.java)
-                intent.putExtra("email", account.email)
-                intent.putExtra("name", account.displayName)
-                startActivity(intent)
-            } else {
-                Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
-            }
-        }
+    private fun updateUI() {
+        requireActivity().startActivity(Intent(context, MapsActivity::class.java))
+        requireActivity().finish()
     }
 
     override fun onDestroyView() {
