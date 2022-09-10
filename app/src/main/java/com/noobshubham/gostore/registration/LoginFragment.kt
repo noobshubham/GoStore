bd671e3370b30cd7c357819d29523bc7346e67b4
noobshubham <noobshubham65@gmail.com>
2022-09-10 17:38:03 +0530

    Minor Fix

--- app/src/main/java/com/noobshubham/gostore/registration/LoginFragment.kt
+++ app/src/main/java/com/noobshubham/gostore/registration/LoginFragment.kt
@@ -3,7 +3,6 @@ package com.noobshubham.gostore.registration
 import android.content.Intent
 import android.content.IntentSender
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
